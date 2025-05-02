a4ff892e7cfb17480322997c3b5af02d38dd5168
tulolo287 <iv1055@yandex.ru>
2025-05-02 19:35:52 +0300

    step 2

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -16,9 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from task_manager import views
+from django.views.generic import TemplateView
+from task_manager.views import IndexView
 
 urlpatterns = [
-    path('', views.index),
+    path('', IndexView.as_view()),
     path('admin/', admin.site.urls),
 ]
